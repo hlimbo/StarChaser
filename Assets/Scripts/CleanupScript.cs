369709ce6e2883bca53690f3b926331faa406ffd
fishies <andrewx2@uci.edu>
2017-11-13 14:20:12 -0800

    Preliminary program hierarchy

---
+++ Assets/Scripts/CleanupScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CleanupScript : MonoBehaviour {
+
+	//ATTACH THIS TO CAMERA
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/MoveWithTime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithTime : MonoBehaviour {
+
+	//adjust position on vertical axis by 1 world-coordinate PER real second.
+	//attach to player and maybe boss
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
