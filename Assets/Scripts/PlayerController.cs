7d2b2fb851e415b6e22fa885f4816c3805c94c12
fishies <andrewx2@uci.edu>
2017-11-13 17:51:54 -0800

    Removed Debug line from PlayerController, experimented with ParticleSystem in scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,7 +18,6 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButton(0)) {
-			Debug.Log (camera.ScreenToWorldPoint (Input.mousePosition));
 			Vector3 position = camera.ScreenToWorldPoint (Input.mousePosition);
 			position.z = 0.0f;
 			transform.position = Vector3.MoveTowards(transform.position, position, Time.deltaTime * speed);
