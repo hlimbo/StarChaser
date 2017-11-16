9fb370a61f9dd0e455156f4fb36a37a1e1529d94
hlimbo <hlimbo@users.noreply.github.com>
2017-11-15 16:02:36 -0800

    Add sensitivity debug bar to game

---
+++ Assets/Scripts/WIP/SensitivityBar.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SensitivityBar : MonoBehaviour {
+    
+    public Text sensitivityText;
+    public MotionController playerControl;
+
+    public void UpdateSensitivityText()
+    {
+        playerControl.movePercent = GetComponent<Slider>().value;
+        sensitivityText.text = playerControl.movePercent.ToString();
+    }
+}
