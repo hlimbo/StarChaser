509fcb425cd7d4f2c32a16292a5022e3efe0d23f
hlimbo <hlimbo@users.noreply.github.com>
2017-11-20 15:44:33 -0800

    Add restartButton to TouchControlsFinal scene

---
+++ Assets/Scripts/Working/RestartGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour {
+
+    public string sceneToReload = "TouchControlsFinal";
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(sceneToReload);
+    }
+}
