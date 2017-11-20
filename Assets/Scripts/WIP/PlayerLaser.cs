1dd3fbde5f564cb61be0ca640270af98d1c61e91
hlimbo <hlimbo@users.noreply.github.com>
2017-11-20 14:13:41 -0800

    Fix broken reference to energy absorber script
    I added a reference to the shield gameObject in the PlayerLaser script to resolve nullreference exception with Shield gameobject.Do note that you cannot find an inactive gameobject in the scene using GameObject.Find(). The game's controls can be found in TouchControlsFinal scene. If you are going to change anything in the scene, please make a copy of it as merge conflicts can occur with unity scene files when multiple people are working on the same scene. Shield needs to be toggled on/off and requires a shield energy bar that slowly regenerates over time when disabled, and depletes when shield is active. Let me handle this part of the code and everyone else needs to focus on creating/designing bullet patterns and movement patterns for the enemies

--- Assets/Scripts/WIP/PlayerLaser.cs
+++ Assets/Scripts/WIP/PlayerLaser.cs
@@ -5,11 +5,11 @@ using UnityEngine.EventSystems;
 public class PlayerLaser : MonoBehaviour {
 
     public GameObject laser;
+    public GameObject shieldRef;
     public EnergyAbsorber EnergyScript;
     private CooldownTimer laserCD;
     private EventTrigger trigger;
     private BoxCollider2D box;
-
 
     void Start ()
     {
@@ -17,8 +17,7 @@ public class PlayerLaser : MonoBehaviour {
         laserCD = GetComponent<CooldownTimer>();
         box = GetComponent<BoxCollider2D>();
 
-        //EnergyScript = GameObject.Find("Shield").GetComponent<EnergyAbsorber>();
-        //For some reason this breaks the laser
+        EnergyScript = shieldRef.GetComponent<EnergyAbsorber>();
 
         //drag and then remove finger off screen to fire laser
         //EventTriggerHelper.AddEvent(trigger, EventTriggerType.EndDrag, Laser);
@@ -26,12 +25,13 @@ public class PlayerLaser : MonoBehaviour {
         EventTriggerHelper.AddEvent(trigger, EventTriggerType.PointerDown, Laser);
 
         //calculate the size of the bounding box collider to be as twice as wide as camera
-        //{
-        //    float camWidth = Camera.main.orthographicSize * Camera.main.aspect * 2f;
-        //    float camHeight = Camera.main.orthographicSize * 2f;
-        //    box.size = new Vector2(camWidth * 2.5f, camHeight);
-        //    box.offset = new Vector2(0f, camHeight / 2f);
-        //}
+        {
+            float camWidth = Camera.main.orthographicSize * Camera.main.aspect * 2f;
+            float camHeight = Camera.main.orthographicSize * 2f;
+            box.size = new Vector2(camWidth * 2.5f, camHeight);
+            box.offset = new Vector2(0f, camHeight / 2f);
+            box.transform.localPosition = new Vector3(0f, 0.65f, 0f);
+        }
     }
     
     void Update ()
@@ -47,11 +47,14 @@ public class PlayerLaser : MonoBehaviour {
     {
         if(!laser.activeInHierarchy && !laserCD.isOnCooldown)
         {
-            //TODO: if EnergyScript.charge == EnergyScript.maxCharge
-            Debug.Log("laser");
-            laserCD.enabled = true;
-            laser.SetActive(true);
-            //todo: calculate laser orientation here
+            if (EnergyScript.charge == EnergyScript.maxCharge)
+            {
+                Debug.Log("laser");
+                laserCD.enabled = true;
+                laser.SetActive(true);
+            }
+
+            //laser orientation ~ may scrap
            // Vector2 tip = transform.position;
            // Vector2 targetPos = Camera.main.ScreenToWorldPoint(data.position);
            // laser.transform.rotation = Quaternion.FromToRotation(Vector3.up, (Vector3)(targetPos - tip));
