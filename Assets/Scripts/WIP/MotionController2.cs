81ae51f0a3ea67526ed9cd20ad5e0e18adfb672a
hlimbo <hlimbo@users.noreply.github.com>
2017-11-28 02:58:22 -0800

    Enlarge ship movement radius and rename ShipPivot to ShipMovableArea in TouchControlsFinal

--- Assets/Scripts/WIP/MotionController2.cs
+++ Assets/Scripts/WIP/MotionController2.cs
@@ -9,7 +9,12 @@ public class MotionController2 : MonoBehaviour {
     private Movement camMovement;
     [SerializeField]
     private bool isFingerOnPivot;
-    Vector3 newShipPos;
+
+    [Tooltip("Measured in unity units per second")]
+    public float speed = 10f;
+    [Tooltip("Measures how many unity units finger/mouse cursor should be below ship. The higher the value the further away finger/mouse cursor is from ship. The lower the value, the closer finger/mouse cursor is from ship.")]
+    public float targetOffset = 0.25f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -20,14 +25,15 @@ public class MotionController2 : MonoBehaviour {
         EventTriggerHelper.AddEvent(trigger, EventTriggerType.PointerUp, (data) => { isFingerOnPivot = false; });
         camMovement = Camera.main.GetComponent<Movement>();
     }
-
     void Update()
     {
         if(isFingerOnPivot)
         {
-            Vector3 newShipPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            newShipPos.z = 0f;
-            transform.position = Vector3.MoveTowards(transform.position, newShipPos, 1.0f);
+            Vector3 targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPos.z = 0f;
+            Vector3 lockPosition = transform.TransformPoint(Vector3.down * targetOffset);
+            Vector3 delta = (targetPos - lockPosition).normalized;
+            transform.position = Vector3.MoveTowards(transform.position, targetPos + Vector3.up * targetOffset, speed * Time.deltaTime);
         }
         else //move ship along with camera if ship isn't being moved by player
         {
