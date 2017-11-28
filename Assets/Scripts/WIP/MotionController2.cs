5fb5d1704d7f52985d1609f9b91207bc659a6f66
hlimbo <hlimbo@users.noreply.github.com>
2017-11-28 01:35:23 -0800

    Use Vector3.MoveTowards instead of rb.MovePosition in MotionController2

--- Assets/Scripts/WIP/MotionController2.cs
+++ Assets/Scripts/WIP/MotionController2.cs
@@ -9,34 +9,27 @@ public class MotionController2 : MonoBehaviour {
     private Movement camMovement;
     [SerializeField]
     private bool isFingerOnPivot;
+    Vector3 newShipPos;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         trigger = GetComponent<EventTrigger>();
-        EventTriggerHelper.AddEvent(trigger, EventTriggerType.PointerDown, FingerDown);
-        EventTriggerHelper.AddEvent(trigger, EventTriggerType.PointerUp, FingerUp);
+        //data is PointerEventData type
+        //onpointerdown is fired only when the cursor/finger is on the shipPivot for the first frame.
+        EventTriggerHelper.AddEvent(trigger, EventTriggerType.PointerDown, (data) => { isFingerOnPivot = true; });
+        EventTriggerHelper.AddEvent(trigger, EventTriggerType.PointerUp, (data) => { isFingerOnPivot = false; });
         camMovement = Camera.main.GetComponent<Movement>();
-    }
-
-    public void FingerDown(PointerEventData data)
-    {
-        Debug.Log("Finger down");
-        isFingerOnPivot = true;
-    }
-
-    public void FingerUp(PointerEventData data)
-    {
-        isFingerOnPivot = false;
     }
 
     void Update()
     {
-        if (isFingerOnPivot)
+        if(isFingerOnPivot)
         {
-            //this will jitter
-            rb.MovePosition(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            Vector3 newShipPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            newShipPos.z = 0f;
+            transform.position = Vector3.MoveTowards(transform.position, newShipPos, 1.0f);
         }
-        else
+        else //move ship along with camera if ship isn't being moved by player
         {
             transform.Translate(camMovement.transform.up * Time.deltaTime * camMovement.speed);
         }
