28ab52ebbe5012f4e7f007ea9a7e247d18d174e8
hlimbo <hlimbo@users.noreply.github.com>
2017-11-27 22:02:59 -0800

    Add camera movement offset to player ship when isn't being moved by the player

--- Assets/Scripts/WIP/MotionController2.cs
+++ Assets/Scripts/WIP/MotionController2.cs
@@ -6,16 +6,38 @@ public class MotionController2 : MonoBehaviour {
 
     private Rigidbody2D rb;
     private EventTrigger trigger;
+    private Movement camMovement;
+    [SerializeField]
+    private bool isShipMoving;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         trigger = GetComponent<EventTrigger>();
         EventTriggerHelper.AddEvent(trigger, EventTriggerType.Drag, MoveShip);
+        EventTriggerHelper.AddEvent(trigger, EventTriggerType.EndDrag, EndDrag);
+        camMovement = Camera.main.GetComponent<Movement>();
+       
+        //move ship when player is moving the ship so the ship stays within camera bounds
+       // rb.MovePosition(rb.position + (Vector2)(camMovement.transform.up * Time.deltaTime * camMovement.speed));
     }
 
     //OnDrag
     public void MoveShip(PointerEventData data)
     {
+        isShipMoving = true;
         rb.MovePosition(Camera.main.ScreenToWorldPoint(data.position));
+    }
+
+    public void EndDrag(PointerEventData data)
+    {
+        isShipMoving = false;
+    }
+
+    void Update()
+    {
+        if(!isShipMoving)
+        {
+            transform.Translate(camMovement.transform.up * Time.deltaTime * camMovement.speed);
+        }
     }
 }
