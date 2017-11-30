96f667fd22f3524275ae1a10185ae61f3100e97b
Wei-yao Hsu <wyhsu@uci.edu>
2017-11-29 18:04:42 -0800

    Created level 1 scene and started mapping out the level designs

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,6 +7,7 @@ public class Movement : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+
     }
     
     // Update is called once per frame
---
+++ Assets/Scripts/Wake.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wake : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("It's finally working");
+        if (col.gameObject.tag == "Enemy")
+        {
+            Movement movementScript = col.gameObject.GetComponent<Movement>();
+            movementScript.enabled = true;
+        }
+    }
+}
