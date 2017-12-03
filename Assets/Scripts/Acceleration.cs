3f945d375eb05efdc48eeda0f4a3b31577d63c8a
fishies <andrewx2@uci.edu>
2017-12-03 03:39:02 -0800

    Implemented Acceleration behavior

---
+++ Assets/Scripts/Acceleration.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acceleration : MonoBehaviour {
+    //Attach this component to any GameObject that has a Movement component.
+    public float acceleration = 0.0f;
+    private Movement movement;
+
+    // Use this for initialization
+    void Start () {
+        movement = GetComponent<Movement> ();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        movement.speed += acceleration * Time.deltaTime;
+    }
+}
