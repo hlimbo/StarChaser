0120b991e0534bddf569c49343101a57ae79af44
fishies <andrewx2@uci.edu>
2017-12-04 05:18:33 -0800

    Updated Emitter to work with Homing and Acceleration behaviors. Does NOT yet work with PredeterminedTurn

--- Assets/Scripts/WIP/Emitter.cs
+++ Assets/Scripts/WIP/Emitter.cs
@@ -11,10 +11,28 @@ public class Emitter : MonoBehaviour {
         [Tooltip("fireRate is projectiles fired per second from this emission point")]
         public float fireRate = 1.0f;
 
-        [Tooltip("'StraightLineBullet' prefab takes 2 args: angle, speed.\n" +
-            "'HomingMissile' prefab takes 3 args: speed, fuel, unfocusRange.\n" +
-            "'ParametricProjectile' prefab takes ")]
-        public float[] args;
+        public float angle = 180.0f;
+        public float speed = 3.0f;
+
+        [Tooltip("Only used if prefab contains Acceleration component.")]
+        public float acceleration = 0.0f;
+
+        [Tooltip("Only used if prefab contains Homing component.")]
+        public float fuel = 2.0f;
+        [Tooltip("Only used if prefab contains Homing component.")]
+        public float unfocusRange = 1.125f;
+
+        [Tooltip("Only used if prefab contains PredeterminedTurn component.")]
+        public bool loopsTurning = false;
+
+        [System.Serializable]
+        public class Turn {
+            public float time;
+            public float angleChange;
+        }
+
+        [Tooltip("Only used if prefab contains PredeterminedTurn component.")]
+        public Turn[] turns;
     }
 
     public EmitterOrigin[] emitters;
@@ -31,8 +49,27 @@ public class Emitter : MonoBehaviour {
         {
             foreach (EmitterOrigin e in emitters) {
                 GameObject bullet = Instantiate<GameObject> (e.projectilePrefab, transform.position + e.offset, Quaternion.identity);
-                bullet.GetComponent<Movement>().speed = e.args[1];
-                bullet.transform.rotation = Quaternion.Euler (0.0f, 0.0f, e.args[0]);
+                bullet.GetComponent<Movement>().speed = e.speed;
+                bullet.transform.rotation = Quaternion.Euler (0.0f, 0.0f, e.angle);
+
+                if (bullet.GetComponent<Acceleration> () != null) {
+                    bullet.GetComponent<Acceleration> ().acceleration = e.acceleration;
+                }
+
+                if (bullet.GetComponent<Homing> () != null) {
+                    bullet.GetComponent<Homing> ().fuel = e.fuel;
+                    bullet.GetComponent<Homing> ().unfocusRange = e.unfocusRange;
+                }
+
+                if (bullet.GetComponent<PredeterminedTurn> () != null) {
+                    bullet.GetComponent<PredeterminedTurn> ().repeating = e.loopsTurning;
+                    bullet.GetComponent<PredeterminedTurn> ().turns = new PredeterminedTurn.Turn[e.turns.Length];
+                    for (int i = 0; i < e.turns.Length; ++i) {
+                        bullet.GetComponent<PredeterminedTurn> ().turns [i].time = e.turns [i].time;
+                        bullet.GetComponent<PredeterminedTurn> ().turns [i].angleChange = e.turns [i].angleChange;
+                    }
+                }
+
                 yield return new WaitForSeconds (1.0f / e.fireRate);
             }
         }
