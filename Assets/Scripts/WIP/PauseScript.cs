045554298ab6920b593d3816d196bb5380930a56
hlimbo <hlimbo@users.noreply.github.com>
2017-12-06 12:42:40 -0800

    Add Pause overlay

---
+++ Assets/Scripts/WIP/PauseScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour {
+
+    void OnEnable()
+    {
+        Time.timeScale = 0f;
+    }
+
+    private void OnDisable()
+    {
+        Time.timeScale = 1f;
+    }
+}
--- Assets/Scripts/Working/ShipMovableArea.cs
+++ Assets/Scripts/Working/ShipMovableArea.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.Assertions;
 using UnityEngine.UI;
 
 //Area the ship can move around is defined to be the entire canvas background image (e.g. entire screen size of mobile device)
@@ -13,39 +12,42 @@ public class ShipMovableArea : MonoBehaviour {
     public GameObject ship;
     [Tooltip("Area in which player vessel cannot move towards or inside of")]
     public GameObject abilityBar;
+    public GameObject pausePanel;
+    public GameObject pauseButton;
 
     private RectTransform abilityBarRect;
+    private RectTransform pauseButtonRect;
     private EventTrigger trigger;
     private Movement camMovement;
     [SerializeField]
     private int fingerMoveId = -1;
 
+    void Awake()
+    {
+        ship = GameObject.FindWithTag("Player");
+        pausePanel = GameObject.Find("PausePanel");
+        abilityBar = GameObject.Find("AbilityBar");
+        pauseButton = GameObject.Find("PauseButton");
+    }
+
     // Use this for initialization
     void Start ()
     {
-        AttachGameObject(out ship, "Player");
-        AttachGameObject(out abilityBar, "AbilityBar");
-
         trigger = GetComponent<EventTrigger>();
         abilityBarRect = abilityBar.GetComponent<RectTransform>();
+        pauseButtonRect = pauseButton.GetComponent<RectTransform>();
         camMovement = Camera.main.GetComponent<Movement>();
 
-        EventTriggerHelper.AddEvent(trigger, EventTriggerType.PointerDown, (data) => { fingerMoveId = (IsTouchingAbilityBar(data.position)) ? -1 : data.pointerId; });
+        EventTriggerHelper.AddEvent(trigger, EventTriggerType.PointerDown, (data) => { fingerMoveId = (IsTouchingUI(data.position)) ? -1 : data.pointerId; });
         EventTriggerHelper.AddEvent(trigger, EventTriggerType.PointerUp, (data) => { fingerMoveId = -1; });
       
     }
 
-    //finds and attaches game object reference to this script
-    private void AttachGameObject(out GameObject gameObject,string tag)
+    //stops movement when UI is clicked
+    private bool IsTouchingUI(Vector2 touchPos)
     {
-        gameObject = GameObject.FindWithTag(tag);
-        Assert.IsNotNull(tag + " is missing in scene");
-    }
-
-    //stops movement when abilitybar is clicked
-    private bool IsTouchingAbilityBar(Vector2 touchPos)
-    {
-        return RectTransformUtility.RectangleContainsScreenPoint(abilityBarRect, touchPos);
+        return RectTransformUtility.RectangleContainsScreenPoint(abilityBarRect, touchPos)
+            || RectTransformUtility.RectangleContainsScreenPoint(pauseButtonRect, touchPos);
     }
 
     private void MoveShip(Vector2 cursorPos)
@@ -60,46 +62,50 @@ public class ShipMovableArea : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
-        if(Input.touchSupported)
+        if (!pausePanel.activeInHierarchy)
         {
-            //only process movement if finger is on screen and finger is not on abilitybar
-            if (Input.touchCount > 0 && fingerMoveId != -1)
+            if (Input.touchSupported)
             {
-                //get correct finger associated with moving the ship
-                Vector2 fingerPos = Vector2.zero;
-                //do this to prevent out of bounds exception (e.g. press shield button first, then press on ship area to move, let go of ship button causes this exception to be thrown)
-                for (int i = 0; i < Input.touchCount; ++i)
+                //only process movement if finger is on screen and finger is not on abilitybar
+                if (Input.touchCount > 0 && fingerMoveId != -1)
                 {
-                    if (Input.GetTouch(i).fingerId == fingerMoveId)
+                    //get correct finger associated with moving the ship
+                    Vector2 fingerPos = Vector2.zero;
+                    //do this to prevent out of bounds exception (e.g. press shield button first, then press on ship area to move, let go of ship button causes this exception to be thrown)
+                    for (int i = 0; i < Input.touchCount; ++i)
                     {
-                        fingerPos = Input.GetTouch(i).position;
-                        break;
+                        if (Input.GetTouch(i).fingerId == fingerMoveId)
+                        {
+                            fingerPos = Input.GetTouch(i).position;
+                            break;
+                        }
                     }
-                }
 
-                MoveShip(fingerPos);
+                    MoveShip(fingerPos);
+                }
+                else //move ship along with camera if ship isn't being moved by player
+                {
+                    ship.transform.Translate(camMovement.transform.up * Time.deltaTime * camMovement.speed);
+                }
             }
-            else //move ship along with camera if ship isn't being moved by player
+            else if (Input.mousePresent) // mouse controls ~ note: known to be glitchy when running on Unity Remote (won't detect touch input and defaults to this behaviour)
             {
-                ship.transform.Translate(camMovement.transform.up * Time.deltaTime * camMovement.speed);
+                //0 = left mouse press
+                //1 = right mouse press
+                //2 = middle mouse press
+
+                if (Input.GetMouseButton(0) && !IsTouchingUI(Input.mousePosition))
+                {
+                    MoveShip(Input.mousePosition);
+                }
+                else //move ship along with camera if ship isn't being moved by player
+                {
+                    ship.transform.Translate(camMovement.transform.up * Time.deltaTime * camMovement.speed);
+                }
             }
+
         }
-        else if (Input.mousePresent) // mouse controls ~ note: known to be glitchy when running on Unity Remote (won't detect touch input and defaults to this behaviour)
-        {
-            //0 = left mouse press
-            //1 = right mouse press
-            //2 = middle mouse press
-            if(Input.GetMouseButton(0) && !IsTouchingAbilityBar(Input.mousePosition))
-            {
-                MoveShip(Input.mousePosition);
-            }
-            else //move ship along with camera if ship isn't being moved by player
-            {
-                ship.transform.Translate(camMovement.transform.up * Time.deltaTime * camMovement.speed);
-            }
-        }
-        
     }
 }
