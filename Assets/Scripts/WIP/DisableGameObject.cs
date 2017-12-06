bb5681e22de4e739e331cc650f3d25b282e8343b
hlimbo <hlimbo@users.noreply.github.com>
2017-12-06 13:55:57 -0800

    Add delay to resume from pause menu

--- Assets/Scripts/WIP/DisableGameObject.cs
+++ Assets/Scripts/WIP/DisableGameObject.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DisableGameObject : MonoBehaviour {
 
-    void Start ()
+    void Awake ()
     {
         gameObject.SetActive(false);
     }
--- Assets/Scripts/WIP/PauseScript.cs
+++ Assets/Scripts/WIP/PauseScript.cs
@@ -1,16 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseScript : MonoBehaviour {
 
-    void OnEnable()
+    public Text countdown;
+    public Text pauseText;
+    public GameObject resumeButton;
+    public bool firstRun = true;//used to stop the game from pausing at the very beginning
+
+    [Tooltip("Amount of seconds before gameplay resumes")]
+    public float delayDuration;
+
+    private IEnumerator Countdown()
     {
-        Time.timeScale = 0f;
+        countdown.enabled = true;
+        pauseText.enabled = false;
+        resumeButton.SetActive(false);
+        float startTime = Time.unscaledTime;
+        float elapsedTime = Time.unscaledTime - startTime;
+        int counter = 0;
+        while (elapsedTime < delayDuration)
+        {
+            Debug.Log("Resuming game in: " + (delayDuration - elapsedTime));
+            yield return new WaitForSecondsRealtime(1f);
+            countdown.text = (delayDuration - ++counter).ToString();
+            elapsedTime = Time.unscaledTime - startTime;
+        }
+
+        pauseText.enabled = true;
+        resumeButton.SetActive(true);
+        Time.timeScale = 1f;
+        gameObject.SetActive(false);
+        yield return null;
     }
 
-    private void OnDisable()
+    public void PauseDelay()
     {
-        Time.timeScale = 1f;
+        StartCoroutine(Countdown());
     }
+
+    void OnEnable()
+    {
+        if (!firstRun)
+        {
+            countdown.text = delayDuration.ToString();
+            countdown.enabled = false;
+            Time.timeScale = 0f;
+        }
+    }
+
+    void OnDisable()
+    {
+        //Time.timeScale = 1f;
+        firstRun = false;
+    }
+
 }
