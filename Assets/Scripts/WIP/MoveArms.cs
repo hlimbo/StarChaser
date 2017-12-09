df2e815814c56fb3c54aec4a89b9b85236f4449a
hlimbo <hlimbo@users.noreply.github.com>
2017-12-08 21:41:11 -0800

    Add Boss Arm Movement

--- Assets/Scripts/WIP/MoveArms.cs
+++ Assets/Scripts/WIP/MoveArms.cs
@@ -4,11 +4,49 @@ using UnityEngine;
 
 public class MoveArms : MonoBehaviour {
 
-    public float speed = 0f;
+    public float changeDirectionDelay;
+    public float moveSpeed;
+    public bool isMovingForwards = true;
+    public Vector3 originalPos;
 
-    // Update is called once per frame
-    void Update ()
+    //use this bool to check if arms moved back to its original position
+    public bool hasReturned
     {
-        transform.Translate(transform.up * Time.deltaTime * speed, Space.World);
+        get
+        {
+            return !isMovingForwards && originalPos == transform.position;
+        }
+    }
+
+    void Awake()
+    {
+        originalPos = transform.position;
+    }
+
+    void Start()
+    {
+        StartCoroutine(ChangeMoveDirectionDelay());
+    }
+
+    void Update()
+    {
+        //stop moving backwards once the center position of the arms moves back to the original position.
+        if (isMovingForwards)
+            transform.Translate(transform.up * -1 * Time.deltaTime * moveSpeed, Space.World);
+        else
+            transform.position = Vector3.MoveTowards(transform.position, originalPos, moveSpeed * Time.deltaTime);
+    }
+
+    public IEnumerator ChangeMoveDirectionDelay()
+    {
+        float startTime = Time.time;
+        float elapsedTime = Time.time - startTime;
+        while (elapsedTime < changeDirectionDelay)
+        {
+            yield return new WaitForSeconds(1f);
+            elapsedTime = Time.time - startTime;
+        }
+        isMovingForwards = false;
+        yield return null;
     }
 }
