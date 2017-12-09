bad0f79bbc11bc7f056bd1ec015c79fe5393746d
hlimbo <hlimbo@users.noreply.github.com>
2017-12-08 20:11:32 -0800

    WIP Arm Rotations

---
+++ Assets/Scripts/WIP/MoveArms.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveArms : MonoBehaviour {
+
+    public float speed = 0f;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        transform.Translate(transform.up * Time.deltaTime * speed, Space.World);
+    }
+}
