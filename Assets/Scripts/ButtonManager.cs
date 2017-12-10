0dd2b71c3a25c1a2b4601c143332ed4e9ed32d81
Kay Lab <kaylab@dhcp-v047-102.mobile.uci.edu>
2017-12-09 19:28:40 -0800

    Added a main menu (mostly complete) as well as empty scenes for Credits and Settings.

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+	public void playButtonClicked(string sceneToLoad) {
+		SceneManager.LoadSceneAsync (sceneToLoad);
+	}
+
+	public void creditsButtonClicked(string sceneToLoad) {
+		SceneManager.LoadSceneAsync (sceneToLoad);
+	}
+
+	public void settingsButtonClicked(string sceneToLoad) {
+		SceneManager.LoadSceneAsync (sceneToLoad);
+	}
+
+	void FixedUpdate() {
+
+	}
+}
