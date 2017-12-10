15f9ddd0d91034b45897c103b2a5fe756b97fa42
Kay Lab <perfectalgorithm@gmail.com>
2017-12-09 22:43:02 -0800

    Fixed the bug on the Credits page which was causing the button to not work properly.

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour {
 	public void switchToScene(string sceneToLoad) {
+		Debug.Log ("clcik");
 		SceneManager.LoadSceneAsync (sceneToLoad);
 	}
 	public void creditsGoBack(string sceneToLoad) {
