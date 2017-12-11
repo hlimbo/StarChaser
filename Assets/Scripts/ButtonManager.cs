eef017a7065118a4cc127f0612bcdb1794e4b5b3
Kay Lab <perfectalgorithm@gmail.com>
2017-12-11 11:15:21 -0800

    Added congratulations screen

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -12,8 +12,11 @@ public class ButtonManager : MonoBehaviour {
 
 
 	public void switchToScene(string sceneToLoad) {
-		DontDestroyOnLoad(GameObject.Find ("button_beep").GetComponent<AudioSource>());
-		GameObject.Find ("button_beep").GetComponent<AudioSource> ().Play ();
+		if (GameObject.Find ("button_beep")) {
+			DontDestroyOnLoad (GameObject.Find ("button_beep").GetComponent<AudioSource> ());
+			GameObject.Find ("button_beep").GetComponent<AudioSource> ().Play ();
+		}
+
         SceneManager.LoadSceneAsync (sceneToLoad);
 		if (!(sceneToLoad == "Credits" || sceneToLoad == "Settings" || sceneToLoad == "MainMenu")) {
 			//Destroy the audio if we're going to play the game
