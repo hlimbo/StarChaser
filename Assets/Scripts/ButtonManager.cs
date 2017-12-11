0df07f0be52bd56309e995dae6f38427b724121d
hlimbo <hlimbo@users.noreply.github.com>
2017-12-11 11:24:20 -0800

    Resize screen size UI

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -7,23 +7,23 @@ public class ButtonManager : MonoBehaviour {
 //Nick: I don't know what you want to keep here so I'll leave these commented out for now
 //I removed the lines I don't need. -- Nick
 
-	GameObject gameObject;
+    GameObject gameObject;
 
 
 
-	public void switchToScene(string sceneToLoad) {
-		if (GameObject.Find ("button_beep")) {
-			DontDestroyOnLoad (GameObject.Find ("button_beep").GetComponent<AudioSource> ());
-			GameObject.Find ("button_beep").GetComponent<AudioSource> ().Play ();
-		}
+    public void switchToScene(string sceneToLoad) {
+        if (GameObject.Find ("button_beep")) {
+            DontDestroyOnLoad (GameObject.Find ("button_beep").GetComponent<AudioSource> ());
+            GameObject.Find ("button_beep").GetComponent<AudioSource> ().Play ();
+        }
 
         SceneManager.LoadSceneAsync (sceneToLoad);
-		if (!(sceneToLoad == "Credits" || sceneToLoad == "Settings" || sceneToLoad == "MainMenu")) {
-			//Destroy the audio if we're going to play the game
-			Destroy(GameObject.Find("main_menu_audio"));
-			Destroy(GameObject.Find("button_beep"));
+        if (!(sceneToLoad == "Credits" || sceneToLoad == "Settings" || sceneToLoad == "MainMenu")) {
+            //Destroy the audio if we're going to play the game
+            Destroy(GameObject.Find("main_menu_audio"));
+            Destroy(GameObject.Find("button_beep"));
 
-		}
-	}
+        }
+    }
 
 }
