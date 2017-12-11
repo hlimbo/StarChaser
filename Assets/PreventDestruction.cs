6302c1a2754c176c6c1dbc14a96f7891ec2f9d60
Kay Lab <perfectalgorithm@gmail.com>
2017-12-10 18:13:11 -0800

    added sound to the main menu, but buggy

---
+++ Assets/PreventDestruction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreventDestruction : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Start() {
+		DontDestroyOnLoad(GetComponent<AudioSource>());
+	}
+}
--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -6,8 +6,26 @@ using UnityEngine.SceneManagement;
 public class ButtonManager : MonoBehaviour {
 //Nick: I don't know what you want to keep here so I'll leave these commented out for now
 //I removed the lines I don't need. -- Nick
-public void switchToScene(string sceneToLoad) {
+
+	GameObject gameObject;
+
+
+	public void Start() {
+		if (GameObject.Find ("main_menu_audio")) {
+			AudioSource audioSource = GameObject.Find ("main_menu_audio").GetComponent<AudioSource>();
+			if (!audioSource.isPlaying) {
+				audioSource.Play ();
+			}
+			Debug.Log (audioSource);
+		}
+
+	}
+	public void switchToScene(string sceneToLoad) {
             SceneManager.LoadSceneAsync (sceneToLoad);
-}
+		if (!(sceneToLoad == "Credits" || sceneToLoad == "Settings")) {
+			//Destroy the audio if it's not on Credits or Settings scene
+			Destroy(GameObject.Find("main_menu_audio"));
+		}
+	}
 
 }
