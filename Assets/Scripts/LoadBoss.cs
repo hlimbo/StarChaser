3ad70f72f937f51ebab299b597a6cd35114ff37a
hlimbo <hlimbo@users.noreply.github.com>
2017-12-11 11:00:53 -0800

    one liner

--- Assets/Scripts/LoadBoss.cs
+++ Assets/Scripts/LoadBoss.cs
@@ -5,15 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class LoadBoss : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
 
     void OnTriggerEnter2D(Collider2D col)
     {
